6d23a5fd85565d0402fb1e014091a7065a1c42d9
Max2002-dbz <maks.kuk20022@gmail.com>
2022-09-26 19:42:51 +0300

    Add project files.

---
+++ CalcClassBr.Tests/CalcClassTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CalcClassBr;
+using System.Data.SqlTypes;
+
+namespace CalcClassBr.Tests
+{
+    [TestClass]
+    public class CalcClassTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                    "testData.xml",
+                    "Operation", DataAccessMethod.Sequential)]
+
+        public void AddTest()
+        {
+            //Arrange
+            long expression_1 = Convert.ToInt64(TestContext.DataRow["expression_1"]);
+            long expression_2 = Convert.ToInt64(TestContext.DataRow["expression_2"]);
+            string expected = Convert.ToString(TestContext.DataRow["expected"]);
+
+            //Act
+            string result = Convert.ToString(CalcClass.Add(expression_1, expression_2));
+            if (CalcClass.lastError != "") result = CalcClass.lastError;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
+
+//public void SubTest()
+//{
+//    //Arrange
+//    long expression_1 = Convert.ToInt64(TestContext.DataRow["expression_1"]);
+//    long expression_2 = Convert.ToInt64(TestContext.DataRow["expression_2"]);
+//    string expected = Convert.ToString(TestContext.DataRow["expected"]);
+
+//    //Act
+//    string result = Convert.ToString(CalcClass.Sub(expression_1, expression_2));
+
+//    Assert.AreEqual(expected, result);
+//}
